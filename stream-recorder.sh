df134d20534542809971c396e5b2c5c7758dfb71
Kuthero <42418700+Kuthero@users.noreply.github.com>
2020-09-16 22:03:59 -0400

    update vars

--- stream-recorder.sh
+++ stream-recorder.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo timedatectl set-timezone America/New_York
+
+#<> Variables here:
+stream='https://player.iheart-us1.zettacloud.com/03767a3b-798d-047f-95dc-637046c2ca7b/d2e6ffc8-963a-27a5-3609-1bce6d2b145e/ksvr-00001'
+timezone='America/New_York'
+segment_time='60' #seconds
+station='tiger-team-radio'
+
+sudo timedatectl set-timezone $timezone
 sleep 3
-sudo docker run --name ffmpeg -d -v /etc/localtime:/etc/localtime:ro -v='/home/ubuntu/audio':/tmp/ffmpeg opencoconut/ffmpeg -i https://player.iheart-us1.zettacloud.com/03767a3b-798d-047f-95dc-637046c2ca7b/d2e6ffc8-963a-27a5-3609-1bce6d2b145e/ksvr-00001 -f segment -strftime 1 -segment_time 60 -reset_timestamps 1 tiger-team-radio-%m-%d_%H-%M-%S.mp3
+sudo docker run --name ffmpeg -d -v /etc/localtime:/etc/localtime:ro -v='/home/ubuntu/audio':/tmp/ffmpeg opencoconut/ffmpeg -i $stream -f segment -strftime 1 -segment_time $segment_time -reset_timestamps 1 $station-%m-%d_%H-%M-%S.mp3
 sleep 3
 sudo docker run --name filebrowser -d -p 80:80 -v /home/ubuntu/audio:/var/www clue/h5ai
 sleep 3
