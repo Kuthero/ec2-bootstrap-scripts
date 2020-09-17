e8b194ff9cf0a270e194f821267b8ae6a70c2247
Kuthero <42418700+Kuthero@users.noreply.github.com>
2020-09-16 20:51:47 -0400

    new stream-recorder

--- script3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo mkdir media
-sleep 300
-sudo docker run -d -p 8000:8000 -v /home/ubuntu/media:/home/streamripper clue/streamripper http://iheart.streamguys1.com:8080/0017 -o version
-sleep 300
-sudo docker run -d -p 80:80 -v /home/ubuntu/media:/var/www clue/h5ai
-sleep 300
